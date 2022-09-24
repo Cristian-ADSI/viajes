6ec52ce32375a40011176365236e86e8ca949eeb
Cristian Otero <cristianoteroadsi@gmail.com>
2022-09-23 22:30:29 -0500

    New ReactRouter version fix

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Search from './pages/Search';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/Hotel-Search',
+    element: <Search />,
+  },
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
-
